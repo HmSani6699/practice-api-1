bd0ba0f241af46554c213ffb22c3b734a76a24c3
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-25 16:08:03 +0600

    complete the car website

--- api-2-data.js
+++ api-2-data.js
@@ -32,34 +32,3 @@ const data = [
 
   },
 ];
-
-const jsonData = JSON.stringify(data);
-console.log(jsonData)
-
-// const displayData = () => {
-//   const container = document.getElementById('container')
-//   data.forEach(car => {
-//     console.log(car);
-//     const div = document.createElement('div');
-//     div.classList.add('card', 'card-compact', 'w-full', 'bg-base-100', 'shadow-xl', 'border',);
-//     div.innerHTML = `
-//           <figure><img class="mt-4"
-//                 src="${car.imageURL ? car.imageURL : "https://i.ibb.co/SJK7QYs/isuzu.png"}"
-//                 alt="Shoes" />
-//             </figure>
-//           <div class="card-body">
-//                <h2 class="card-title">
-//                Name : ${car.name}</h2>
-//                   <p><span class="font-bold">Car details :</span> ${car.description}
-//                   </p>
-//               <div
-//                   class="card-actions ">
-//                   <button
-//                       class="btn btn-primary">Car price : ${car.price}</button>
-//               </div>
-//           </div>
-//     `;
-//     container.appendChild(div)
-//   })
-// }
-// displayData()
---
+++ car.js
@@ -0,0 +1,33 @@
+const loadData = async () => {
+    const res = await fetch('http://127.0.0.1:5500/data.json');
+    const data = await res.json();
+    displayData(data)
+}
+
+const displayData = (cars) => {
+    const container = document.getElementById('container')
+    cars.forEach(car => {
+        console.log(car);
+        const div = document.createElement('div');
+        div.classList.add('card', 'card-compact', 'w-full', 'bg-base-100', 'shadow-xl', 'border',);
+        div.innerHTML = `
+          <figure><img class="mt-4"
+                src="${car.imageURL ? car.imageURL : "https://i.ibb.co/SJK7QYs/isuzu.png"}"
+                alt="Shoes" />
+            </figure>
+          <div class="card-body">
+               <h2 class="card-title">
+               Name : ${car.name}</h2>
+                  <p><span class="font-bold">Car details :</span> ${car.description}
+                  </p>
+              <div
+                  class="card-actions ">
+                  <button
+                      class="btn btn-primary">Car price : ${car.price}</button>
+              </div>
+          </div>
+    `;
+        container.appendChild(div)
+    })
+}
+loadData()
